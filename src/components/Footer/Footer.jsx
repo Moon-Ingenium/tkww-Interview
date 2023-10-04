8d6daa5b322765a8c04a181a0c8c701a3be80d95
Oriol Martí <oriol.marti.collell@viaplaygroup.com>
2023-10-04 15:08:53 +0200

    Adding footer component

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import './Footer.css';
+
+export default function Footer() {
+    return (
+        <footer>
+            <div className='container'>
+                <h3>Interview Footer</h3>
+                <p>Built with love</p>
+            </div>
+        </footer>
+    );
+}
