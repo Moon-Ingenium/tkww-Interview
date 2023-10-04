ddf158c2df87c26ac5d78b956136e78bd12e5e91
Oriol Martí <oriol.marti.collell@viaplaygroup.com>
2023-10-04 15:09:00 +0200

    Adding Header component

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import './Header.css';
+
+export default function Header({ children }) {
+    return (
+        <header>
+            <div className='container'>
+                <h1 className='text'>Interview Header</h1>
+                {children}
+            </div>
+        </header>
+    );
+}
