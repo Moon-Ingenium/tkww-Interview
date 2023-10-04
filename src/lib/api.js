57063caaed804382983f6d10d8bbc09e36abeeae
Oriol Martí <oriol.marti.collell@viaplaygroup.com>
2023-10-04 18:52:40 +0200

    Reset commit

--- src/lib/api.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios'
-
-async function getProducts() {
-    return await axios.get('https://qa-registry-interview-api.regsvcs.theknot.com/products')
-}
-
-const API = { getProducts }
-
-export default API;
